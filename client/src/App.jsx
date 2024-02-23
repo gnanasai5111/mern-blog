00536070e70833110f03a816b452398eab09e293
gnanasai <gnanasai5111@gmail.com>
2024-02-23 15:18:00 +0530

    "set up server"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
-    <div><h1 className='text-3xl text-red-500'>dhdh</h1></div>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
